5b06374b37ce0d4a8d0901fde259ad8f253d8110
Michael <mball.28@outlook.com>
2019-04-17 09:16:53 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let css = document.querySelector('h3');
+
+let color1 = document.querySelector('#color1');
+let color2 = document.querySelector('#color2');
+const body = document.getElementById('gradient');
+
+// console.log(color1);console.log(color2);console.log(css)
+let gradient = () => {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
+
+color1.addEventListener('input', gradient);
+color2.addEventListener('input', gradient);
+
+// color1.addEventListener('input', () => {
+//     console.log(color1.value);
+// })
+// color2.addEventListener('input', () => {
+//     console.log(color2.value);
+// })
